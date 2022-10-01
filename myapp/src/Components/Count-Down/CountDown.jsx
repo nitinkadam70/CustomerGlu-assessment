b4076c4574a26bf673a735501ef14bf816c8483d
Nitin Kadam <99539536+nitinkadam70@users.noreply.github.com>
2022-10-02 01:23:05 +0530

    Logic part is done

--- myapp/src/Components/Count-Down/CountDown.jsx
+++ myapp/src/Components/Count-Down/CountDown.jsx
@@ -1,14 +1,63 @@
 import React from 'react';
-import { useState } from 'react';
-import Stopwatch from '../../CustomHook/Stopwatch';
+import { useState, useRef, useEffect } from 'react';
 import styles from '../../Styles/timer.module.css';
+import TimerLimitInput from './TimerLimitInput';
 
+//Reusable Count-Down component
 const CountDown = () => {
-  const [timerLimit, setTimerLimit] = useState(0);
-  const { seconds, start, pause, reset } = Stopwatch(timerLimit);
+  const timerId = useRef(null);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [hour, setHour] = useState(0);
+  // console.log(seconds, minutes);
+  const start = () => {
+    if (!timerId.current) {
+      timerId.current = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    }
+  };
+  const pause = () => {
+    clearInterval(timerId.current);
+    timerId.current = null;
+  };
+  const reset = () => {
+    clearInterval(timerId.current);
+    timerId.current = null;
+    setSeconds(0);
+    setMinutes(0);
+    setHour(0);
+  };
+
+  useEffect(() => {
+    if (seconds === 59) {
+      setMinutes(minutes + 1);
+      setSeconds(0);
+    }
+    if (minutes === 59 && seconds === 59) {
+      setHour(1);
+      setMinutes(0);
+      setSeconds(0);
+    }
+  }, [seconds, minutes, hour]);
+
+  //Clean Up Function
+  //becouse whenever our component is unmounted then it reset this four value
+  //1 - timerId.current = null;
+  //2 - setSeconds(0);
+  //3 - setMinutes(0);
+  //4 -  setHour(0);
+  useEffect(() => {
+    return () => {
+      clearInterval(timerId.current);
+    };
+  }, []);
   return (
     <div className={styles.countDown}>
-      <h1> {seconds}</h1>
+      <TimerLimitInput />
+      <h1>
+        {hour}:{minutes}:{seconds}
+      </h1>
       <button onClick={start}>Start</button>
       <button onClick={pause}>Pause</button>
       <button onClick={reset}>Reset</button>
---
+++ myapp/src/Components/Count-Down/TimerLimitInput.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import styles from '../../Styles/timerLimitInputs.module.css';
+const TimerLimitInput = () => {
+  const [timerLimit, setTimerLimit] = useState({});
+
+  const handleChange = (e) => {
+    setTimerLimit({
+      ...timerLimit,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const validations = (e) => {
+    if (e.target.value.length > 2)
+      e.target.value = e.target.value.slice(0, e.target.maxLength);
+  };
+
+  return (
+    <div className={styles.inputsContainer}>
+      <div>
+        <input
+          onInput={validations}
+          onChange={handleChange}
+          placeholder="00"
+          type="number"
+          name="hour"
+        />
+        <p>h</p>
+      </div>
+      <div>
+        <input
+          onInput={validations}
+          onChange={handleChange}
+          placeholder="00"
+          type="number"
+          name="minutes"
+        />
+        <p>m</p>
+      </div>
+      <div>
+        <input
+          onInput={validations}
+          onChange={handleChange}
+          placeholder="00"
+          type="number"
+          name="seconds"
+        />
+        <p>s</p>
+      </div>
+    </div>
+  );
+};
+
+export default TimerLimitInput;
